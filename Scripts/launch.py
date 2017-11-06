6c08f8d431850839924a134588e42d7802c7ccd6
Wendy JANDIN <wendy.jandin@ensimag.grenoble-inp.fr>
2017-11-06 14:42:34 +0100

    avancée du script launch

--- Scripts/launch.py
+++ Scripts/launch.py
@@ -1,5 +1,14 @@
 import os
 
-# Importation des données
+# Importation des donnees
 os.system('RequeteAromeHD.py 0 SP1')
 os.system('mv *.grib2 Donnees')
+
+# Exportation au format nc
+os.system('./wgrib2 *.grib2 -netcdf MesDonnees.nc')
+
+# Traitement sur Paraview
+# permet d'avoir des .png
+
+
+# Exportation au format KML
