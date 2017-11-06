14bbc9d21336f1161952e865819dad6f03e1ebb1
Wendy JANDIN <wendy.jandin@ensimag.grenoble-inp.fr>
2017-11-06 14:54:54 +0100

    lauch fonctionne jusqu'au png

--- Scripts/launch.py
+++ Scripts/launch.py
@@ -1,14 +1,14 @@
 import os
 
 # Importation des donnees
-os.system('RequeteAromeHD.py 0 SP1')
+os.system('python RequeteAromeHD.py 0 SP1')
 os.system('mv *.grib2 ../Donnees')
 
 # Exportation au format nc
-os.system('./wgrib2 Donnees/*.grib2 -netcdf ../Donnees/MesDonnees.nc')
+os.system('../wgrib2 ../Donnees/*.grib2 -netcdf ../Donnees/MesDonnees.nc')
 
 # Traitement sur Paraview
 # permet d'avoir des .png
-os.system('visuAvecTemperature.py ../Donnees/MesDonnees.nc')
+os.system('pvpython VisuAvecTemperature.py ../Donnees/MesDonnees.nc')
 
 # Exportation au format KML
