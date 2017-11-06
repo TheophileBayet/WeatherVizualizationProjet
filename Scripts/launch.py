7462b2f6ae4a7d79cd17fec1e26a25168b2f1b2d
Wendy JANDIN <wendy.jandin@ensimag.grenoble-inp.fr>
2017-11-06 14:47:58 +0100

    avancees de launch.py

--- Scripts/launch.py
+++ Scripts/launch.py
@@ -5,10 +5,10 @@ os.system('RequeteAromeHD.py 0 SP1')
 os.system('mv *.grib2 Donnees')
 
 # Exportation au format nc
-os.system('./wgrib2 *.grib2 -netcdf MesDonnees.nc')
+os.system('./wgrib2 Donnees/*.grib2 -netcdf Donnees/MesDonnees.nc')
 
 # Traitement sur Paraview
 # permet d'avoir des .png
-
+os.system('visuAvecTemperature.py Donnees/MesDonnees.nc')
 
 # Exportation au format KML
