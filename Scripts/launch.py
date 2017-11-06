0325b2799eeddfa7f72dc0cacd11e15ba65fb8e4
Theophile Bayet <theophile.bayet@ensimag.grenoble-inp.fr>
2017-11-06 16:59:07 +0100

    modifs du script lauch pour générer les trois cartes - à modif + KML multiples overlays à modif

--- Scripts/launch.py
+++ Scripts/launch.py
@@ -7,9 +7,11 @@ os.system('mv *.grib2 ../Donnees')
 # Exportation au format nc
 os.system('../wgrib2 ../Donnees/*.grib2 -netcdf ../Donnees/MesDonnees.nc')
 
-#Suppression des données grib2 parasites 
+#Suppression des donnees grib2 parasites
 os.system('rm ../Donnees/*.grib2')
 
 # Traitement sur Paraview
 # permet d'avoir des .png
-os.system('pvpython VisuAvecTemperature.py ../Donnees/MesDonnees.nc')
+os.system('pvpython temperatures.py ../Donnees/MesDonnees.nc')
+#os.system('pvpython isovaleurs.py ../Donnees/MesDonnees.nc')
+os.system('pvpython lignes_courant.py ../Donnees/MesDonnees.nc')
