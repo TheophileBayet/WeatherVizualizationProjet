683f582af06a8e943ad3120347b5c0b1ad4d2b4d
Theophile Bayet <theophile.bayet@ensimag.grenoble-inp.fr>
2017-11-17 15:30:37 +0100

    coucou c'est la fin

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,34 +1,31 @@
-Consignes du TP : 
+Projet de Visualisation scientifique réalisé par : 
+-Jandin Wendy
+-Bayet Théophile
+
+Utilisation : 
+	    Dossiers : 
+	    	     /Scripts : Les scripts pythons utilisés pour gérer les données (téléchargement, traitement, stockage).
+		     /KML : Les scripts KML utilisés pour visualiser les données sur google earth.
+		     /Images : Les images générées par les scripts pythons.
+		     /Donnees : Les données qui ne sont pas supprimées.
+
+	    Lancement : 
+	    	      cd /Scripts 
+
+		      Le script launch.py peut prendre 0, 1 ou 2 arguments : 
+		      -python launch.py génère 3 images dans le dossier /Images, correspondant au traitement des dernières données de météoFrance ( requeteHD 0 SP1 ). Ces images peuvent être affichées via le script KML affichage_carte.kml.
+ 
+		      -python launch.py X génère 3*X images dans le dossier /IMages, ainsi qu'un script generated_script dans /KML, ce dernier permettant d'afficher via une animation l'évolution prévue par les données météoFrance sur le laps de temps X. ( exemple : python launch.py 3 )
+ 
+		      -python launch.py X Y génère juste les 3 images correspondant au traitement de la donnée X dans les prévisions de météofrance, et les place dans le dossier /Images. 'Y' peut être n'importe quoi, cela importe peu. ( exemple : python launch.py 6 bis  // python launch.py 5 youplaboum ) 
 
 
+            Affichage sur Google Earth : 
+	    	      Lancer le script launch.py comme décrit précédemment afin d'obtenir les données que vous souhaitez. 
+		      Ouvrir le script affichage_carte.kml pour afficher les données uniquement pour les dernières en dates.
+		      Ouvrir le script generated_script.kml pour afficher une animation des prévisions de météoFrance sur le temps que vous avez choisi en lançant le script python.
 
-  1  importation et lecture des données
-  2  implémentation de cartes de couleur pour les données scalaires
-  3  implémentation de l'algorithme de calcul de courbes iso-valeurs
-  4  implémentation de l'algorithme de calcul de lignes de courant
-  5  exportation des sorties des algorithmes dans des fichiers au format KML (Keyhole Markup Language), pour leur visualisation dans le logiciel Google Earth.
-
-
-Méthode agile : 
-
-1ere étape : importation de données météo-france, carte de couleur, sortie au format KML pour visualisation sur Google Earth. 
-	- Importation & lecture des données 
-	- Exportation au format KML 
-	- Visualisation sur Google Earth 
-
-2eme étape : ajout des courbes iso-valeurs 
-     - Ajout des courbes iso-valeur 
-     - Exportation au format KML 
-     - visualisation sur Google Earth
-
-3eme étape : 
-     - Ajout des lignes de courant 
-     - Exportation au format KML 
-     - Visualisation sur Google Earth. 
-
-
-DONE. 
-
-TODO : 
--Améliorer contraste
--Animations ! 
+Bonne utilisation.
+Questions : 
+wendy.jandin@grenoble-inp.org
+theophile.bayet@grenoble-inp.org
--- Scripts/launch.py
+++ Scripts/launch.py
@@ -12,7 +12,7 @@ def KML_ScriptGeneration(Iterations):
         script_kml.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n")
         script_kml.write("<kml xmlns=\"http://www.opengis.net/kml/2.2\"> \n")
         script_kml.write("<Folder> \n")
-        script_kml.write("<name>France temperature informations at time "+ str(i) +" </name> \n")
+        script_kml.write("<name>Temperature and wind informations at time "+ str(i) +" </name> \n")
         script_kml.write("<description>Ground overlay on France of temps and wind streams at time : "+str(i)+" </description> \n")
         while i<NbrPrevisions+1 :
 
@@ -21,7 +21,7 @@ def KML_ScriptGeneration(Iterations):
             script_kml.write("<begin>2017-11-"+str(16+i)+"</begin>\n")
             script_kml.write("<end>2017-11-"+str(16+i+1)+"</end>\n")
             script_kml.write("</TimeSpan> \n")
-            script_kml.write("<name>France temperatures at time "+str(i)+"</name> \n")
+            script_kml.write("<name>Temperatures at time "+str(i)+"</name> \n")
             script_kml.write("<Icon> \n")
             script_kml.write("<href><![CDATA[../Images/MesDonnees_"+str(i)+".nc_temp.png]]></href> \n")
             script_kml.write("</Icon> \n")
@@ -39,7 +39,7 @@ def KML_ScriptGeneration(Iterations):
             script_kml.write("<begin>2017-11-"+str(16+i)+"</begin>\n")
             script_kml.write("<end>2017-11-"+str(16+i+1)+"</end>\n")
             script_kml.write("</TimeSpan> \n")
-            script_kml.write("<name>France temperatures isovaleurs at time "+str(i)+" </name> \n")
+            script_kml.write("<name>Isovalues of temperatures at time "+str(i)+" </name> \n")
             script_kml.write("<Icon> \n")
             script_kml.write("<href><![CDATA[../Images/MesDonnees_"+str(i)+".nc_isovaleurs.png]]></href> \n")
             script_kml.write("</Icon> \n")
@@ -57,7 +57,7 @@ def KML_ScriptGeneration(Iterations):
             script_kml.write("<begin>2017-11-"+str(16+i)+"</begin>\n")
             script_kml.write("<end>2017-11-"+str(16+i+1)+"</end>\n")
             script_kml.write("</TimeSpan> \n")
-            script_kml.write("<name>France wind streams at time "+str(i)+"</name> \n")
+            script_kml.write("<name>Wind streams at time "+str(i)+"</name> \n")
             script_kml.write("<Icon> \n")
             script_kml.write("<href><![CDATA[../Images/MesDonnees_"+str(i)+".nc_courants.png]]></href> \n")
             script_kml.write("</Icon> \n")
@@ -83,7 +83,7 @@ if __name__ == "__main__":
 
     #  Lancement du script avec arguments
     if (len(sys.argv) == 2 ):
-        os.system('echo .        ===  recuperation et traitement des donnees de meteo France ===')
+        os.system('echo .        ===  Recuperation et traitement des donnees de meteo France ===')
         os.system('echo ')
         NbrPrevisions = int(sys.argv[1])
         i=0
@@ -118,10 +118,40 @@ if __name__ == "__main__":
         #Exportation au format nc
 
 
+    if(len(sys.argv)==3):
+            NbrPrevisions = int(sys.argv[1])
+            os.system('echo         ===  Recuperation de la donnee '+str(NbrPrevisions)+' de meteo France ===')
+            os.system('python RequeteAromeHD.py '+str(NbrPrevisions)+' SP1')
+            os.system('mv *.grib2 ../Donnees')
+            os.system('echo ')
+            # Exportation au format nc
+            os.system('echo         === Exportation de la donnee au format nc ===')
+            os.system('echo ')
+            os.system('../wgrib2 ../Donnees/*.grib2 -netcdf ../Donnees/MesDonnees_'+str(NbrPrevisions)+'.nc')
+            os.system('echo ')
+            #Suppression des donnees grib2 parasites
+            os.system('rm ../Donnees/*.grib2')
+
+            # Traitement sur Paraview
+            # permet d'avoir des .png
+            os.system('echo ')
+            os.system('echo         === Generation de l\'image dans le dossier Images ===')
+            os.system('echo ')
+            os.system('echo 1. Generation des temperatures')
+            os.system('pvpython temperatures.py ../Donnees/MesDonnees_'+str(NbrPrevisions)+'.nc')
+            os.system('echo 2. Generation des isovaleurs')
+            os.system('pvpython isovaleurs.py ../Donnees/MesDonnees_'+str(NbrPrevisions)+'.nc')
+            os.system('echo 3. Generation des lignes de courant')
+            os.system('pvpython lignes_courant.py ../Donnees/MesDonnees_'+str(NbrPrevisions)+'.nc')
+            os.system('mv ../Donnees/*.png ../Images')
+
+            os.system('echo ')
+            os.system('echo             === Fin du script ===')
+            sys.exit(1)
     #Lancement du script sans arguments : affichage de base
     if (len(sys.argv) == 1):
             #Importation des donnees
-            os.system('echo         ===  recuperation des donnees de meteo France ===')
+            os.system('echo         ===  Recuperation des donnees de meteo France ===')
             os.system('python RequeteAromeHD.py 0 SP1')
             os.system('mv *.grib2 ../Donnees')
             os.system('echo ')
